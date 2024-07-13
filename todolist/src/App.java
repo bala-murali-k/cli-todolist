751903eedb9f3ef92885881495f560dcfa204b79
BalaMurali0701 <k.balamurali0701@gmail.com>
2024-07-13 18:01:23 +0530

    Implemented full frame for termux

--- todolist/src/App.java
+++ todolist/src/App.java
@@ -1,13 +1,26 @@
 import java.io.Console;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
 
         Service service = new Service();
         Console console = System.console();
+        Scanner scan = new Scanner(System.in);
 
         service.clearScreen();
         service.welcomeScreen();
         console.readPassword("Press Enter to continue....");
+        while(true) {
+
+            service.clearScreen();
+            service.display();
+
+            System.out.print("Press N/n to exit....");
+            String exit = scan.nextLine();
+            if(exit.equals("n")) break;
+        }
+
+        scan.close();
     }
 }
--- todolist/src/Service.java
+++ todolist/src/Service.java
@@ -11,7 +11,7 @@ public class Service {
     }
 
     public void welcomeScreen(){
-        System.out.println("+----------------------------------------------+");
+        System.out.println("+-----------------------------------------------+");
         System.out.println("| ___________                ________           |");
         System.out.println("| \\__    ___/___             \\______ \\   ____   |");
         System.out.println("|   |    | /  _ \\    ______   |    |  \\ /  _ \\  |");
@@ -26,5 +26,53 @@ public class Service {
         System.out.println("|                 \\/       \\/                   |");
         System.out.println("|                                               |");
         System.out.println("+-----------------------------------------------+");
-    };
+    }
+
+    public void display(){
+
+        int height = 15;
+        int width = 49;
+        String frame_size[][] = new String[height][width];
+
+        int top_start = 0;
+        int top_end = 49;
+        int bottom_start = 15;
+        int bottom_end = 49;
+
+        for (int i=0; i<height; i++) {
+            for (int j=0; j<width; j++) {
+                frame_size[i][j] = " ";
+            }
+        }
+
+        for (int i=1; i<top_end-1; i++) {
+            frame_size[top_start][i] = "-";
+        }
+
+        for (int i=1;i<bottom_end-1;i++) {
+            frame_size[bottom_start-1][i] = "-";
+        }
+
+        for (int i=1; i<bottom_start-1; i++) {
+            frame_size[i][top_start] = "|";
+        }
+
+        for (int i=1; i<bottom_start-1; i++) {
+            frame_size[i][bottom_end-1] = "|";
+        }
+
+        frame_size[top_start][top_start] = "+";
+        frame_size[top_start][top_end-1] = "+";
+        frame_size[bottom_start-1][top_start] = "+";
+        frame_size[bottom_start-1][bottom_end-1] = "+";
+
+        for (int i=0; i<height; i++) {
+            for (int j=0; j<width; j++) {
+                System.out.print(frame_size[i][j]);
+            }
+            System.out.println("");
+        }
+
+    }
+
 }
