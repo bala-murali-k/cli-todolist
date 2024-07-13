789f1f4e924014d4babc19c586fa870822f0cb6c
BalaMurali0701 <k.balamurali0701@gmail.com>
2024-07-13 15:18:11 +0530

    Implementing welcome screen 2

--- todolist/src/Service.java
+++ todolist/src/Service.java
@@ -13,15 +13,15 @@ public class Service {
     public void welcomeScreen(){
         System.out.println("+----------------------------------------------+");
         System.out.println("| ___________                ________           |");
-        System.out.println("| \\__    ___/___             \\______ \   ____   |");
-        System.out.println("|   |    | /  _ \    ______   |    |  \\ /  _ \\  |");
+        System.out.println("| \\__    ___/___             \\______ \\   ____   |");
+        System.out.println("|   |    | /  _ \\    ______   |    |  \\ /  _ \\  |");
         System.out.println("|   |    |(  <_> )  /_____/   |    `   (  <_> ) |");
         System.out.println("|   |____| \\____/            /_______  /\\____/  |");
         System.out.println("|                                    \\/         |");
         System.out.println("|         .____    .__          __              |");
         System.out.println("|         |    |   |__| _______/  |_            |");
         System.out.println("|         |    |   |  |/  ___/\\   __\\           |");
-        System.out.println("|         |    |___|  |\___ \\  |  |             |");
+        System.out.println("|         |    |___|  |\\___ \\  |  |             |");
         System.out.println("|         |_______ \\__/____  > |__|             |");
         System.out.println("|                 \\/       \\/                   |");
         System.out.println("|                                               |");
